62bad2eae5f85bd615ecd871df0821ae06fd1c12
imarkevych-student <marke_im25@student.itstep.org>
2025-06-18 21:17:28 +0300

    Workers

---
+++ 05_StructRefOut_Classwork/Program.cs
@@ -0,0 +1,67 @@
+﻿//Описати клас з ім'ям Worker, що містить наступні поля:
+//прізвище та ініціали працівника;
+//вік працівника;
+//ЗП працівника;
+//дата прийняття на роботу.
+//Написати програму, що виконує наступні дії:
+//введення з клавіатури даних в масив, що складається з п'яти елементів типу Worker (записи повинні бути впорядковані за алфавітом);
+//якщо значення якогось параметру введено не в відповідному форматі - згенерувати відповідний exception.
+//вивід на екран прізвища працівника, стаж роботи якого перевищує введене з клавіатури значення.
+//Рекомендація: перевірку формата даних та генерацію винятку виконуйте в блоці set{} для кожної властивості класа Worker. 
+using System;
+namespace _05_StructRefOut_Classwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Worker[] workers  = new Worker[1];
+
+            for (int i = 0; i < workers.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine($"Введіть дані працівника {i + 1}:");
+                    Console.Write("Прізвище:");
+                    string lastName = Console.ReadLine();
+                    Console.Write("Ім`я:");
+                    string firstName = Console.ReadLine();
+                    Console.Write("Вік:");
+                    int age = int.Parse(Console.ReadLine());
+                    Console.Write("Зарплата:");
+                    decimal salary = decimal.Parse(Console.ReadLine());
+                    Console.Write("Дата прийняття на роботу (yyyy-MM-dd): ");
+                    DateTime hireDate;
+                    while (!DateTime.TryParseExact(Console.ReadLine(), "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out hireDate) || hireDate > DateTime.Now)
+
+
+                    {
+                        Console.WriteLine("Помилка! Введіть правильну дату у форматі yyyy-MM-dd, яка не є у майбутньому:");
+                    }
+
+                    workers[i] = new Worker(lastName, firstName, age, salary, hireDate);
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine($"Помилка: {ex.Message}");
+                }    
+
+                
+            }
+
+            Console.Write("Введіть мінімальний стаж роботи:");
+            int minExperience = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Працівники зі стажем більше {minExperience} років:");
+
+            foreach (Worker worker in workers) 
+            {
+                if (worker.GetWorkExperience() > minExperience) 
+                {
+                    Console.WriteLine(worker.WorkerName);
+                }
+            }
+        }
+    }
+}
---
+++ 05_StructRefOut_Classwork/Worker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _05_StructRefOut_Classwork
+{
+    public struct FullName
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public FullName( string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public override string ToString() 
+        { 
+            return $"{LastName} \t{FirstName}";
+        }
+    }
+    public class Worker
+    {
+        public FullName WorkerName { get; set; }
+        private int age;
+        private decimal salary;
+        private DateTime hireDate;
+
+        public int Age 
+        {
+            get {return age;}
+            set 
+            {
+                if (value < 18 || value > 100)
+                {
+                    throw new ArgumentException("Вік працівника має бути від 18 до 100 років.");
+                }                    
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+            set 
+            {
+                if (value <=0)
+                {
+                    throw new ArgumentException("Зарплата має бути додатним числомю");
+                }
+                salary = value;
+            }
+        }
+        public DateTime HireDate
+        {
+            get { return hireDate; }
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException("Дата прийняття на роботу не може бути в майбутньому");
+                }
+            }
+        }
+
+        public Worker(string lastName, string firstName,  int age, decimal salary, DateTime hireDay)
+        {
+            WorkerName = new FullName(firstName, lastName);
+            Age = age;
+            Salary = salary;
+            HireDate = hireDay;
+        }
+
+        public int GetWorkExperience()
+        {
+            return DateTime.Now.Year - HireDate.Year;
+        }
+    }
+}
