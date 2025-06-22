0d64f6e41fc17975d971e8e7d423d8415ba84475
imarkevych-student <marke_im25@student.itstep.org>
2025-06-22 19:05:21 +0300

    Freezer - homework

---
+++ 04_01_IntroOOP_Classwork/FreezerMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_01_IntroOOP_Classwork
+{
+    public partial class Freezer
+    {
+        public static int GetTotalFreezers()
+        {
+            return totalFreezers;
+        }
+
+        public static void SetDefaultBrand(string brand)
+        {
+            if (!string.IsNullOrEmpty(brand))
+            {
+                defaultBrand = brand;
+            }
+            else
+            {
+                defaultBrand = "GenericBrand";
+            }
+        }
+
+    }
+}
