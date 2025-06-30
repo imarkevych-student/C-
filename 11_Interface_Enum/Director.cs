4bfdbd0c0fcc60897c8c3dea2c517efb07098234
imarkevych-student <marke_im25@student.itstep.org>
2025-06-30 20:54:04 +0300

    Classwork

---
+++ 11_Interface_Enum/Director.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Interface_Enum
+{
+    internal class Director : ICloneable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime BirthYear { get; set; }
+
+        public object Clone()
+        {
+            return new Director
+            {
+                FirstName = this.FirstName,
+                LastName = this.LastName,
+                BirthYear = this.BirthYear,
+            };
+        }
+
+        public override string ToString() 
+        {
+            return $"{FirstName} {LastName} (born {BirthYear})";
+        }
+    }
+}
---
+++ 11_Interface_Enum/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Interface_Enum
+{
+    public enum Genre
+    {
+        Action,
+        Comedy,
+        Drama,
+        Horror,
+        SciFi,
+        Documentary,
+        Romance
+
+    }
+    internal class Movie: ICloneable, IComparable
+    {
+        public string Title {  get; set; }
+        public string Description { get; set; }
+        public Director Director { get; set; }
+        public string Country { get; set; }
+        public  Genre Genre { get; set; }
+        public int Year {  get; set; }
+        public double Raiting { get; set; }
+
+
+    }
+}
