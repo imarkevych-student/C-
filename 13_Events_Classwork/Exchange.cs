1fbdae11c0444da10882960b8f5c7bd6e513dbda
imarkevych-student <marke_im25@student.itstep.org>
2025-07-06 08:28:11 +0300

    Classwork

---
+++ 13_Events_Classwork/Exchange.cs
@@ -0,0 +1,37 @@
+﻿namespace _13_Events_Classwork
+{
+    public delegate void RateChangedHandler(decimal newRate);    
+    public delegate void MaxRateReachedHandler(decimal maxRate);    
+    public delegate void MinRateReachedHandler(decimal minRate);
+    internal class Exchange
+    {
+        private decimal _currentUsdtoUah;
+        private decimal _maxRate = decimal.MinValue;
+        private decimal _minRate = decimal.MaxValue;
+        private Random _random = new Random();
+
+        public event RateChangedHandler RateChanged;
+        public event MaxRateReachedHandler MaxRateReached;
+        public event MinRateReachedHandler MinRateReached;
+
+
+        public void SimulateRateChange()
+        {
+            _currentUsdtoUah = Math.Round((decimal)(_random.NextDouble() * 5 + 40), 2);
+            Console.WriteLine($"\n[Біржа] Новий курс USD/UAH: {_currentUsdtoUah} ₴");
+
+            RateChanged?.Invoke(_currentUsdtoUah);
+
+            if (_currentUsdtoUah > _maxRate)
+            {
+                _maxRate = _currentUsdtoUah;
+                MaxRateReached?.Invoke(_currentUsdtoUah);
+            }
+            if (_currentUsdtoUah < _minRate)
+            {
+                _minRate = _currentUsdtoUah;
+                MinRateReached?.Invoke(_currentUsdtoUah);
+            }
+        }
+    }
+}
---
+++ 13_Events_Classwork/Trader.cs
@@ -0,0 +1,51 @@
+﻿namespace _13_Events_Classwork
+{
+    internal class Trader
+    {
+        public string Name { get; }
+        public decimal HryvniaBalance   { get; private set; }
+        public decimal DollarBalance { get; private set; }
+        public decimal BuyUsdBelow { get; }
+        public decimal SellUsdAbove { get; }
+
+        public Trader(string name, decimal hryvnia, decimal buyUsdBelow, decimal sellUsdAbove)
+        {
+            Name = name;
+            HryvniaBalance = hryvnia;            
+            BuyUsdBelow = buyUsdBelow;
+            SellUsdAbove = sellUsdAbove;
+        }
+
+        public void Subscribe(Exchange exchange)
+        {
+            exchange.RateChanged += OnRateChanged;            
+        }
+
+        private void OnRateChanged(decimal usdToUah)
+        {
+            if (usdToUah <= BuyUsdBelow && HryvniaBalance >= usdToUah)
+            {
+                int dollarsToBuy = (int)(HryvniaBalance / usdToUah);
+                decimal totalCost = dollarsToBuy * usdToUah;
+
+                DollarBalance += dollarsToBuy;
+                HryvniaBalance -= totalCost;
+
+                Console.WriteLine($"[{Name}] Купив ${dollarsToBuy} за {usdToUah} ₴/USD. Баланс: {HryvniaBalance:F2} ₴, {DollarBalance} $");
+            }
+            else if(usdToUah >= SellUsdAbove && DollarBalance >=1)
+            {                
+                decimal totalRevenue = DollarBalance * usdToUah;
+                Console.WriteLine($"[{Name}] Продав ${DollarBalance} за {usdToUah} ₴/USD. Баланс: {HryvniaBalance + totalRevenue:F2} ₴, 0 $");
+
+                HryvniaBalance += totalRevenue;
+                DollarBalance = 0;
+            }
+            else
+            {
+                Console.WriteLine($"[{Name}] Немає можливості купити або продати. Поточний курс: {usdToUah} ₴/$");
+
+            }
+        }
+    }
+}
