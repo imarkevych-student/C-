fdefd9b4434c9890feff3afeb01809c7538bed44
imarkevych-student <marke_im25@student.itstep.org>
2025-07-14 20:52:38 +0300

    classwork

---
+++ 19_05_Classwork/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _19_05_Classwork
+{
+    internal class Program
+    {
+        static Dictionary<int, User> users = new();
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 19_05_Classwork/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using System.Text.Json;
+
+
+namespace _19_05_Classwork
+{
+    internal class User
+    {
+        [Required(ErrorMessage = "ID не заданий")]
+        [Range(1000,9999, ErrorMessage = "ID має бути в діапазоні 1000-9999")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Login не заданий")]
+        [RegularExpression("^[A-Za-z0-9]+$", ErrorMessage = "Login має містити тільки букви та цифри")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Name не заданий")]
+        [EmailAddress(ErrorMessage ="Некоректний Email")]
+        public string Email { get; set; }
+
+        [CreditCard(ErrorMessage = "Невалідна картка")]
+        public string CreditCard { get; set; }
+
+        [Required(ErrorMessage = "Пароль не заданий")]
+        [MinLength(8, ErrorMessage = "Пароль має бути не менше 8 символів")]
+        [RegularExpression("^[A-Za-z0-9]+$", ErrorMessage = "Пароль має містити тільки букви та цифри")]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "Паролі не співпадають")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Phone(ErrorMessage = "Невірний формат телефону")]
+        [RegularExpression(@"\+38-0\d{2}-\d{3}-\d{2}-\d{2}", ErrorMessage = "Телефон має бути у форматі +38-0XX-XXX-XX-XX")]
+        public string Phone { get; set; }       
+    }
+}
